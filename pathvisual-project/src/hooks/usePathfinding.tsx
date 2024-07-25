87700b3165e5550216d93efae15985f1e6745133
Chhavesh <chhaveshpatle@gmail.com>
2024-07-26 00:44:16 +0530

    Built Pathfinding Context

--- pathvisual-project/src/hooks/usePathfinding.tsx
+++ pathvisual-project/src/hooks/usePathfinding.tsx
@@ -1,19 +1,12 @@
 import { useContext } from "react";
-import { PathfindingContext  } from "../context/PathfindingContext";
+import { PathfindingContext } from "../context/PathfindingContext";
 
+export const usePathfinding = () => {
+  const context = useContext(PathfindingContext);
 
-export const usePathfinding =() => {
-    const context = useContext(PathfindingContext);
+  if (!context) {
+    throw new Error("usePathfinding must be used within a PathfindingProvider");
+  }
 
-    if(!context){
-        throw new Error("usePathfinding must be used within a PathfindingContext");
-    }
-      // Use the destructured properties directly
-    // const grid = context.grid;
-    // const setGrid = context.setGrid;
-    // const algorithm = context.algorithm;
-    // const setAlgorithm = context.setAlgorithm;
-
-    // // Or, you can return the entire context object
-    return ;
+  return context.usePathfinding;
 };
